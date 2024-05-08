9a2caccce99a5ca32313eb50eae99ebffd217785
Nikhil Thorat <nkhthorat@gmail.com>
2024-05-08 11:26:24 +0200

    Model Trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact','train.csv')
@@ -54,4 +54,8 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
